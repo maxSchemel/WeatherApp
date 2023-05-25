c854421b6ceac8d052617943fc404ba4cec80c17
Maximilian_Schemel <maximilian@ub-schemel.de>
2023-05-25 07:25:46 +0200

    Commit #4: - Stage before introduction of weatherAPI

--- Weather App/task/web/app.py
+++ Weather App/task/web/app.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 import sys
+import secrets
 
 app = Flask(__name__)
+secret = secrets.token_urlsafe(32)
+app.secret_key = secret
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///weather.db"
 db = SQLAlchemy(app)
 
@@ -30,14 +33,23 @@ def index():
 def load_city():
     if request.method == 'POST':
         if city.query.filter(city.name == request.form["city_name"]).first():
-            return redirect(url_for('index'))
+            flash("The city has already been added to the list!")
+            return redirect(url_for('index', message="The city has already been added to the list!"))
         print(f"City {request.form['city_name']} is added to database")
+
         city_weather = city(request.form["city_name"], "Warm", 25)
         db.session.add(city_weather)
         db.session.commit()
         return redirect(url_for('index'))
     if request.method == 'GET':
         return redirect(url_for('index'))
+
+@app.route('/delete/<city_id>', methods=['GET', 'POST'])
+def delete(city_id):
+    del_city = city.query.filter_by(id=city_id).first()
+    db.session.delete(del_city)
+    db.session.commit()
+    return redirect('/')
 
 
 # don't change the following way to run flask:
